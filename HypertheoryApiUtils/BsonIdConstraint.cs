b154466f0e8b327348ff2ec630aaaef0ffbf3dee
Jeffry Gonzalez <jeff@hypertheory.com>
2022-04-21 15:33:20 -0400

    docs

--- HypertheoryApiUtils/BsonIdConstraint.cs
+++ HypertheoryApiUtils/BsonIdConstraint.cs
@@ -5,6 +5,8 @@ namespace HypertheoryApiUtils;
 
 public class BsonIdConstraint : IRouteConstraint
 {
+    
+    // used in your program.cs to add a constraint for bson ids.
     public bool Match(HttpContext? httpContext, IRouter? route, string routeKey, RouteValueDictionary values, RouteDirection routeDirection)
     {
         if (values.TryGetValue(routeKey, out var routeValue))
