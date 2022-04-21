95a69695d1e16f57c10067c97271984849ebd8af
Jeffry Gonzalez <jeff@hypertheory.com>
2022-04-21 15:39:09 -0400

    Added example for Route Contraint

--- HypertheoryApiUtils/BsonIdConstraint.cs
+++ HypertheoryApiUtils/BsonIdConstraint.cs
@@ -5,8 +5,25 @@ namespace HypertheoryApiUtils;
 
 public class BsonIdConstraint : IRouteConstraint
 {
-    
-    // used in your program.cs to add a constraint for bson ids.
+
+    /// <summary>
+    /// Creates a route constraint that verifies if a route value is a BsonId or not. Add it to your route constraints .
+    /// </summary>
+    /// <example>
+    /// This is how you add it to your constraints.
+    /// <code>
+    /// builder.Services.AddRouting(options =>
+    ///{
+    ///    options.ConstraintMap.Add("bsonid", typeof(BsonIdConstraint));
+    ///});
+    /// </code>
+    /// </example>
+    /// <param name="httpContext"></param>
+    /// <param name="route"></param>
+    /// <param name="routeKey"></param>
+    /// <param name="values"></param>
+    /// <param name="routeDirection"></param>
+    /// <returns></returns>
     public bool Match(HttpContext? httpContext, IRouter? route, string routeKey, RouteValueDictionary values, RouteDirection routeDirection)
     {
         if (values.TryGetValue(routeKey, out var routeValue))
